4994009545e9f3449d92c5131380cbbf01cda613
uzzal71 <uzzalroy.acm@gmail.com>
2023-01-03 00:09:51 +0600

    Create simple signup page

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/signup', (req, res) => {
+  res.send('Welcome sign page')
+})
+
 app.get('/login', (req, res) => {
   res.send('Welcome login page')
 })
